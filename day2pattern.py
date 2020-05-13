5a300fffa266e00c42a9a92f7e3a31fb96d9b9ba
anushreetrivedi01 <65077358+anushreetrivedi01@users.noreply.github.com>
2020-05-13 21:07:34 +0530

    day 2 code for pattern

---
+++ day2pattern.py
@@ -0,0 +1,14 @@
+for i in range(5):
+for j in range(10):
+if j<5-ior j>=5+i:
+print("  *",end="")
+else:
+print(   ", end="")
+print()
+for i in range(5):
+for j in range(10):
+if j<=ior j>=9-i:
+print("  *"' end="")
+else:
+(print("   ", end=""))
+print()
