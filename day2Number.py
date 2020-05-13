0b41920857963476f9705acf3da971c7d90d9df1
anushreetrivedi01 <65077358+anushreetrivedi01@users.noreply.github.com>
2020-05-13 20:57:37 +0530

    day 2 number code

---
+++ day2Number.py
@@ -0,0 +1,33 @@
+n=int(input("enter a number :"))
+c=0
+arm=0
+rev=0
+if n%2==0
+print("number is even")
+else:
+print("number is odd")
+for x in range (1,n):
+if n%x==0:
+c+=1
+if c>1:
+break
+if c>1:
+print("number is not prime")
+else:
+print("number is prime")
+copy=n
+while n>0:
+d=n%10
+arm+=(d*d*d)
+rev=rev*10
+rev++d
+n=n//10
+
+if rev==copy:
+print(number is palindrome")
+else:
+print("number is not palindrome")
+if arm==copy:
+print("number is armstrong")
+else:
+print("number is not armstrong")
