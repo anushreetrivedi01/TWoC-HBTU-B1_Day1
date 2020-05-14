afe32fef213ab675783b139bea3632b8624d719c
anushreetrivedi01 <65077358+anushreetrivedi01@users.noreply.github.com>
2020-05-14 19:23:22 +0530

    day 3 code 3 done

---
+++ day3code3.py
@@ -0,0 +1,7 @@
+def duplicate(string):
+duplicateString=""
+for x in string:
+if x not in duplicateString + x
+return duplicate String
+S=input("enter a string:")
+print("after removing a duplicate character in the string, a new string is:",duplicate(S))
