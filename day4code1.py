780588786fa6681be8391006e4c40e419ebb4b65
anushreetrivedi01 <65077358+anushreetrivedi01@users.noreply.github.com>
2020-05-16 16:10:42 +0530

    day 4 code 1 final

--- day4code1.py
+++ day4code1.py
@@ -4,3 +4,10 @@ for ele in tup:
 if(ele == x):
 count=count +1
 return count
+tup = (10,8,5,2,10,15,10,8,5,8,8,2)
+enq = 4
+enq1 = 10
+enq2 = 8
+print(Count(tup,enq),"times")
+print(Count(tup,enq1),"times")
+print(Count(tup,enq2),"times")
