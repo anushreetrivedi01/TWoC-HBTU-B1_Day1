9f06fcc21958237aeead51c4cfcf5608b77a0be8
anushreetrivedi01 <65077358+anushreetrivedi01@users.noreply.github.com>
2020-05-17 19:22:28 +0530

    day 6 code 12 done

---
+++ day6code12.py
@@ -0,0 +1,21 @@
+s1=input("enter string1 :")
+s2=input("enter any string 2 :")
+l1=[]
+l2=[]
+c=""
+l1=list(s1)
+l2=list(s2)
+if l1<l2:
+for x in l1:
+for y in l2:
+ifx==y:
+c=c+x
+print(c)
+elif l2<l1:
+for x in l2:
+for y in l1:
+if x==y:
+c=c+x
+print(c)
+elif l2==l1
+print(l1)
