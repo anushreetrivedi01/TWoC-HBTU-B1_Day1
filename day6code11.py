05effb35a732715d76cefeb4da4453e05b1ce859
anushreetrivedi01 <65077358+anushreetrivedi01@users.noreply.github.com>
2020-05-17 19:17:33 +0530

    day 6 code 11 done

---
+++ day6code11.py
@@ -0,0 +1,17 @@
+def maxpdt(l,n):
+m=[]
+for i in range(n):
+for j in range(i+1,n):
+for k in range(j+1,n):
+mk=l[i]*l[j]*l[k]
+m.append
+print(max(m))
+
+
+s=int(input("enter the size of the list :"))
+a=[]
+for i in range(s):
+le=int(input("enter the list element :"))
+a.append(le)
+print(a)
+maxpdt(a,s)
