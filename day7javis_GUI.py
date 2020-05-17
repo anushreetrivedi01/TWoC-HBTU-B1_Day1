3d9d0514e130b6ca4213622c2ef36d06089f26da
anushreetrivedi01 <65077358+anushreetrivedi01@users.noreply.github.com>
2020-05-17 23:12:01 +0530

    day 7 javis_GUI FINAL

--- day7javis_GUI.py
+++ day7javis_GUI.py
@@ -25,61 +25,59 @@ x += 1
 
 clock = pygame.time.Clock()
 clock.tick(10)
-while
+while pygame.mixer.music.get_busy():
+pygame.event.poll()
+clock.tick(10)
+
+def recognizeSpeech():
+r=sr.Recognizer()
+with sr.Microphone() as source:
+print("speak...")
+audio=r.listen(source)
+print("heard...")
+data=""
 
 
+try:
+data=r.recognize_google(audio)
+priny("you said :" + data)
+except sr.UnknownValueError:
+print(google speech recognition could not understand audio")
+except sr.RequestError as e:
+print("could not request results from google search recognition service; {0}".format(e))
+return data
 
 
+def jarvis(data):
+speak("i am fine")
+
+elif "what time is it" in data:
+speak(time())
+
+elif "where is" in data:
+data=data.split(" ")
+location= data[2]
+speak("hold on anushree,i will show you where" + location + "is.")
+wb.open_new_tab("https://www.google.nl/maps/place/" + location + "&amp;")
+else:
+speak(",,,,,,,i did not get what you said!")
 
 
+      
+      root = Tk()
+      
+      rootr.title("Assistant")
+      root.geometry("400*400")
+      x=0
+      
+      w=Label(root,text = "hi,what can we do for youGHT ?",takefocus = True,font=").pack(side=LEFT)
+              
+              but2 = Button(text="quit", command=root.quit, activebackground= "white",bg = "red", fg= "white",height=2,width=10).pack(side = RIGHT)
+              but1 = Button(text = 'listen','bd=4,command= recordAudio,height=2,width=10,activebackground= "lightreen").pack(side= RIGHT)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      
+      
+      
+      root.mainloop()
+      
+      
